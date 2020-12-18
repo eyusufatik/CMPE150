aae64daf1b8f0b5cea4a386dc57fc0892e0bd37b
eyusufatik <esadyusufatik@gmail.com>
2020-12-18 10:49:42 +0300

    old hws

---
+++ HW_BMI/bmi_hw.py
@@ -0,0 +1,12 @@
+temp_celcius=5
+
+weight=60
+
+height=100
+
+##############DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE ##########
+temp_fahrenheit = (temp_celcius * 1.8) + 32.0
+bmi = float(weight) / (float(height)/100.0)**2
+print(temp_fahrenheit)
+print(bmi)
+##############DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE##########
---
+++ HW_SUM/sum_hw.py
@@ -0,0 +1,9 @@
+number1 = float(input("first number: "))
+number2 = float(input("second number: "))
+
+sum = number1 + number2
+
+if int(sum) == sum:
+    print(int(sum))
+else:
+    print(sum)
